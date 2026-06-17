0d21d44c984250a793f36da9f520751a558f7378
Diyor-net <dgwww2222@gmail.com>
2026-06-18 01:38:39 +0500

    server ishga tushdi

--- src/cfg.js
+++ src/cfg.js
@@ -1,1 +1,1 @@
-export const API_LINK = 'http://localhost:5000/api', SITE_LINK = 'http://localhost:5000';
+export const API_LINK = 'http://167.233.133.120/api', SITE_LINK = 'http://167.233.133.120';
